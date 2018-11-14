e48d47e36bb6750bfa8310c02d95390e8664952e
Bartlomiej Gocwinski <bartlomiej.gocwinski@gmail.com>
2018-11-14 23:54:57 +0100

    lesson #6

--- helloProgram/Program.cs
+++ helloProgram/Program.cs
@@ -4,20 +4,63 @@ namespace helloProgram
 {
     class Program
     {
-        private const string textPhrase = "My name is: ";
+        private const string askForName = "What is your name: ";
+        private const string askForAge =  "What is your age: ";
 
         static void Main(string[] args)
         {
-            string myName = args[0];
+            int age;
+            for (; ;)
+            {
+                string name = NewMethod();
+                age = NewMethod1(name);
 
-            Console.WriteLine(textPhrase + myName);
-            Console.WriteLine("Data: " + DateTime.Now.ToString("dd/mm/yyyy"));
-            Console.WriteLine("Czas: " + (DateTime.Now.ToString("HH:mm:ss")));
+                ClearProgramData();
+            }
 
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine(textPhrase + myName);
-            //}
+
+        }
+
+        private static int NewMethod1(string name)
+        {
+            int age;
+            Console.Write(askForAge);
+            bool ageResult = int.TryParse(Console.ReadLine(), out age);
+
+            Console.WriteLine("Hi, " + name +
+                                "\nDzisiejsza data to: " + DateTime.Now.ToString("dddd, dd MMMM yyyy"));
+
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jestes pełnoletni !");
+            }
+            else if (ageResult == false)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Wprowadziłeś błędny wiek !");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Nie jestes pełnoletni !");
+            }
+
+            return age;
+        }
+
+        private static void ClearProgramData()
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        private static string NewMethod()
+        {
+            Console.Write(askForName);
+            string name = Console.ReadLine();
+            return name;
         }
     }
 }
